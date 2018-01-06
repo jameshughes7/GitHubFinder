e3b91981189f30614591a03c917d52ad8abf2482
Tom Spencer <tomspencer_uk@yahoo.co.uk>
2018-01-06 23:38:37 +0000

    Fixed getUser request by including user name to URL and moving client ID and client Secret to paramaters rather than custom headers

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const axios = require('axios');
+const axios = require("axios");
+
+var url = "https://api.github.com/users";
+
 
 module.exports = {
-  getUser(username) {
-    return axios
-      .get(`https://api.github.com/users/${username}`)
-      .then(res => res.data)
-      .catch(error => console.log(error));
-  }
+ getUser(username) {
+   return axios
+     .get(url + `/${username}`, {client_id: "833ab42dcfdbbdd0664a",
+     client_secret: "a0cdae701a5e1bb6d1f73aa6b60f71b2acb79412"})
+     .then(res => res.data)
+     .catch(error => console.log(error));
+ }
 };
