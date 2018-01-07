4460f114e9dee6f9ef42b872b69b578ac389cd0c
Tom Spencer <tomspencer_uk@yahoo.co.uk>
2018-01-07 14:40:34 +0000

    Correct params and headers call for axios.get

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ var url = "https://api.github.com/users";
 module.exports = {
  getUser(username) {
    return axios
-     .get(url + `/${username}`, {client_id: "833ab42dcfdbbdd0664a",
-     client_secret: "a0cdae701a5e1bb6d1f73aa6b60f71b2acb79412"})
+     .get(url + `/${username}`, {
+         params: {
+           client_id: "833ab42dcfdbbdd0664a",
+           client_secret: "a0cdae701a5e1bb6d1f73aa6b60f71b2acb79412"
+         },
+         headers: {
+
+         }   
+    })
      .then(res => res.data)
      .catch(error => console.log(error));
  }
